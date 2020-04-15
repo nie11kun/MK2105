6b112547b1d3271927b997853c0b2cc7ae3eac56
Marco Nie <github@niekun.net>
2020-04-15 20:57:20 +0800

    add plc

---
+++ PLC/oem_alarms_plc_eng.ts
@@ -0,0 +1,262 @@
+<!DOCTYPE TS>
+<TS>
+	<context>
+		<name>slaeconv</name>
+		<message>
+		<source>700000/PLC/PMC</source>
+		<translation>Emergency stop of machine tool,please check the status of emergency stop button SB0！(I1/I32.0)</translation>
+		</message>
+		<message>
+		<source>700001/PLC/PMC</source>
+		<translation>QF1_Guide lubrication pump motor overload！(I7/I32.6)</translation>
+		</message>
+		<message>
+		<source>700002/PLC/PMC</source>
+		<translation>QF2_Hydraulic pump motor overload！(I9/I33.0)</translation>
+		</message>
+		<message>
+		<source>700003/PLC/PMC</source>
+		<translation>QF3_External cooling pump motor overload！(I11/I33.2)</translation>
+		</message>
+		<message>
+		<source>700004/PLC/PMC</source>
+		<translation>QF4_Internal cooling pump motor overload！(I12/I33.3)</translation>
+		</message>
+		<message>
+		<source>700005/PLC/PMC</source>
+		<translation>QF5_Finishing cooling pump motor overload！(I13/I33.4)</translation>
+		</message>
+		<message>
+		<source>700006/PLC/PMC</source>
+		<translation>QF6_Fog absorber motor overload！(I14/I33.5)</translation>
+		</message>
+		<message>
+		<source>700007/PLC/PMC</source>
+		<translation>QF7_The water-cooled machine is overloaded！(I15/I33.6)</translation>
+		</message>
+		<message>
+		<source>700008/PLC/PMC</source>
+		<translation>QF8_Electrical box ac motor overload！(I17/I34.0)</translation>
+		</message>
+		<message>
+		<source>700009/PLC/PMC</source>
+		<translation>QF9_Cooling tank cooling device overload！(I19/I34.2)</translation>
+		</message>
+		<message>
+		<source>700010/PLC/PMC</source>
+		<translation>QF10_Dc power overload！(I21/I34.4)</translation>
+		</message>
+		<message>
+		<source>700011/PLC/PMC</source>
+		<translation>QF11_Control transformer overload！(I22/I34.5)</translation>
+		</message>
+		<message>
+		<source>700012/PLC/PMC</source>
+		<translation>QF14_Grinding wheel motor overload！(I23/I34.6)</translation>
+		</message>
+		<message>
+		<source>700013/PLC/PMC</source>
+		<translation>QF15_The roller motor is overloaded！(I25/I35.0)</translation>
+		</message>
+		<message>
+		<source>700014/PLC/PMC</source>
+		<translation>QF16_AR lubrication pump motor overload！(I27/I35.2)</translation>
+		</message>
+		<message>
+		<source>700015/PLC/PMC</source>
+		<translation>QF17_System drive power overload！(I42/I37.1)</translation>
+		</message>
+		<message>
+		<source>700016/PLC/PMC</source>
+		<translation>ALARM1_Wheel spindle brake unit alarm！(I6/I32.5)</translation>
+		</message>
+		<message>
+		<source>700132/PLC/PMC</source>
+		<translation>KP12_Cooling pump 1 cooling water flow is insufficient！(I68/I40.3)</translation>
+		</message>
+		<message>
+		<source>700133/PLC/PMC</source>
+		<translation>KP13_Cooling pump 2 cooling water flow is insufficient！(I69/I40.4)</translation>
+		</message>
+		<message>
+		<source>700134/PLC/PMC</source>
+		<translation>KP14_Cooling pump 3 cooling water flow is insufficient！(I70/I40.5)</translation>
+		</message>
+		<message>
+		<source>700020/PLC/PMC</source>
+		<translation>ALARM5_Cooling tank level alarm！(I20/I34.3)</translation>
+		</message>
+		<message>
+		<source>700021/PLC/PMC</source>
+		<translation>ALARM6_Grinding wheel spindle frequency converter alarm！(I24/I34.7)</translation>
+		</message>
+		<message>
+		<source>700022/PLC/PMC</source>
+		<translation>ALARM7_Roller motor driver alarm！(I26/I35.1)</translation>
+		</message>
+		<message>
+		<source>700023/PLC/PMC</source>
+		<translation>ALARM8_The drive is not ready！(I28/I35.3)</translation>
+		</message>
+		<message>
+		<source>700024/PLC/PMC</source>
+		<translation>ALARM9_Power module temperature alarm！(I29/I35.4)</translation>
+		</message>
+		<message>
+		<source>700025/PLC/PMC</source>
+		<translation>ALARM10_Spindle motor temperature alarm！(I41/I37.0)</translation>
+		</message>
+		<message>
+		<source>700026/PLC/PMC</source>
+		<translation>ALARM11_Spindle lubrication pump oil level alarm！(I43/I37.2)</translation>
+		</message>
+		<message>
+		<source>700027/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700028/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700029/PLC/PMC</source>
+		<translation>KP3_Spindle lubrication pump pressure loop low pressure！(I48/I37.7)</translation>
+		</message>
+		<message>
+		<source>700030/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700031/PLC/PMC</source>
+		<translation>Out of step program output error！</translation>
+		</message>
+		<message>
+		<source>700032/PLC/PMC</source>
+		<translation>There is a fault in the water-cooling circuit 1 of the machine tool！(I44/I37.3)</translation>
+		</message>
+		<message>
+		<source>700033/PLC/PMC</source>
+		<translation>There is a fault in the water-cooling circuit 2 of the machine tool！(I45/I37.4)</translation>
+		</message>
+		<message>
+		<source>700034/PLC/PMC</source>
+		<translation>There is a fault in the water-cooling circuit 3 of the machine tool！(I46/I37.5)</translation>
+		</message>
+		<message>
+		<source>700035/PLC/PMC</source>
+		<translation>There is a fault in the water-cooling circuit 4 of the machine tool！(I47/I37.6)</translation>
+		</message>
+		<message>
+		<source>700036/PLC/PMC</source>
+		<translation>There is a fault in the water-cooling circuit 5 of the machine tool！(I62/I39.5)</translation>
+		</message>
+		<message>
+		<source>700037/PLC/PMC</source>
+		<translation>There is a fault in the water-cooling circuit 6 of the machine tool！(I63/I39.6)</translation>
+		</message>
+		<message>
+		<source>700038/PLC/PMC</source>
+		<translation>There is a fault in the water-cooling circuit 7 of the machine tool！(I64/I39.7)</translation>
+		</message>
+		<message>
+		<source>700039/PLC/PMC</source>
+		<translation>Cooling tank leak！(I56/I38.7)</translation>
+		</message>
+		<message>
+		<source>700040/PLC/PMC</source>
+		<translation>Fire extinguisher alarm！(I57/I39.0)</translation>
+		</message>
+		<message>
+		<source>700041/PLC/PMC</source>
+		<translation>UPS alarm！</translation>
+		</message>
+		<message>
+		<source>700042/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700043/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700044/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700045/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700046/PLC/PMC</source>
+		<translation>The system is not enabled, please press "Feed enable" to enable the system！</translation>
+		</message>
+		<message>
+		<source>700047/PLC/PMC</source>
+		<translation>Please start the hydraulic pump in JOG mode first！</translation>
+		</message>
+		<message>
+		<source>700048/PLC/PMC</source>
+		<translation>Hand wheel interference is effective, please handle with care！</translation>
+		</message>
+		<message>
+		<source>700049/PLC/PMC</source>
+		<translation>DRF is valid and is manually interfering with X1 state, please note！</translation>
+		</message>
+		<message>
+		<source>700050/PLC/PMC</source>
+		<translation>DRF is valid and is manually interfering with X10 state, please note！</translation>
+		</message>
+		<message>
+		<source>700051/PLC/PMC</source>
+		<translation>DRF is valid and is manually interfering with X100 state, please note！</translation>
+		</message>
+		<message>
+		<source>700052/PLC/PMC</source>
+		<translation>The X and Z axis of the machine are not returned to zero, please return to zero！</translation>
+		</message>
+		<message>
+		<source>700053/PLC/PMC</source>
+		<translation>The feed rate of the machine is zero！</translation>
+		</message>
+		<message>
+		<source>700054/PLC/PMC</source>
+		<translation>You can cut off the main power supply of the machine(QS)！</translation>
+		</message>
+		<message>
+		<source>700055/PLC/PMC</source>
+		<translation>Spindle lubrication pump is being lubricated for the first time, please do not open the grinding wheel！</translation>
+		</message>
+		<message>
+		<source>700056/PLC/PMC</source>
+		<translation>ALARM4_Electric box air conditioning alarm！(I18/I34.1)</translation>
+		</message>
+		<message>
+		<source>700057/PLC/PMC</source>
+		<translation>KP1_The hydraulic oil pressure is insufficient！(I10/I33.1)</translation>
+		</message>
+		<message>
+		<source>700058/PLC/PMC</source>
+		<translation>ALARM2_Guide oil level alarm！(I8/I32.7)</translation>
+		</message>
+		<message>
+		<source>700059/PLC/PMC</source>
+		<translation>KP2_Machine pressure is too low, please check the air supply circuit！(I32/I35.7)</translation>
+		</message>
+		<message>
+		<source>700060/PLC/PMC</source>
+		<translation>ALARM3_Cooling water level alarm！(I16/I33.7)</translation>
+		</message>
+		<message>
+		<source>700061/PLC/PMC</source>
+		<translation>The guard door is unlocked！</translation>
+		</message>
+		<message>
+		<source>700062/PLC/PMC</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700063/PLC/PMC</source>
+		<translation></translation>
+		</message>
+	</context>
+</TS>
